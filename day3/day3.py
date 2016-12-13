c42de7dae920b355f7c5740caefaf6fcec3bb8de
Phuc-T-Tran <phuc978@gmail.com>
2016-12-13 04:14:28 -0500

    Finished Day 3 part one and switched to Python

--- day3/day3.py
+++ day3/day3.py
@@ -5,8 +5,7 @@ for line in file:
 	line = line.split()           # line -> list of numbers (as strings)
 	line = [int(x) for x in line] # convert string ints to ints
 	line.sort()
-
-	if(line[0] + line[1] < line[2]):
+	if(line[0] + line[1] > line[2]):
 		count += 1
 print(count)
 
