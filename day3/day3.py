bb2a86cd8c87fc4d6b4f50a4859091eee81575ea
Phuc Tran <phuc978@gmail.com>
2016-12-13 14:44:26 -0500

    Now converting the string directly into a list of ints in one line

--- day3/day3.py
+++ day3/day3.py
@@ -2,10 +2,9 @@ file = open("input.txt")
 
 count = 0
 for line in file:
-	line = line.split()           # line -> list of numbers (as strings)
-	line = [int(x) for x in line] # convert string ints to ints
-	line.sort()
-	if(line[0] + line[1] > line[2]):
+	line = [int(x) for x in line.split()] # convert string to list of ints
+	line.sort()                           # and sort in increasing order
+	if(line[0] + line[1] > line[2]):      # valid triangle if sum of smaller sides > bigger side
 		count += 1
 print(count)
 
