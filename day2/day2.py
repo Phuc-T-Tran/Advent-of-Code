eb689f19d729829c5ad95c96810feaf482f5c763
Phuc Tran <phuc978@gmail.com>
2016-12-16 19:24:10 -0500

    Uploaded correct file

--- day2/day2.py
+++ day2/day2.py
@@ -6,14 +6,13 @@ r, c = 1, 1
 file = open("input.txt")
 for line in file:
 	for char in line:
-		print(char)
-		if (char == 'U')
+		if (char == 'U'):
 			r = max(0, r - 1)
-	# 	else if (char == 'D')
-	# 		r = min(2, r + 1)
-	# 	else if (char == 'L')
-	# 		c = max(0, c - 1)
-	# 	else if (char == 'R')
-	# 		c = min(2, c + 1)
-	print(keypad[r][c])
+		elif (char == 'D'):
+			r = min(2, r + 1)
+		elif (char == 'L'):
+			c = max(0, c - 1)
+		elif (char == 'R'):
+			c = min(2, c + 1)
+	print(keypad[r][c], end="")
 file.close()
