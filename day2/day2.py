1861de1429e8656f48c8c52b7c5ef85bfcb3efcd
Phuc Tran <phuc978@gmail.com>
2016-12-23 20:15:14 -0500

    Added comments

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,4 @@
+# Initialize the keypad and starting position
 keypad = [[1, 2, 3],
           [4, 5, 6],
           [7, 8, 9]]
@@ -5,15 +6,17 @@ r, c = 1, 1
 
 file = open("input.txt")
 for line in file:
+	# Update current position based on character read
 	for char in line:
-		print(char)
-		if (char == 'U')
+		if (char == 'U'):
 			r = max(0, r - 1)
-	# 	else if (char == 'D')
-	# 		r = min(2, r + 1)
-	# 	else if (char == 'L')
-	# 		c = max(0, c - 1)
-	# 	else if (char == 'R')
-	# 		c = min(2, c + 1)
+		elif (char == 'D'):
+			r = min(2, r + 1)
+		elif (char == 'L'):
+			c = max(0, c - 1)
+		elif (char == 'R'):
+			c = min(2, c + 1)
+
+	# Print the new position after reading the current line
 	print(keypad[r][c])
 file.close()
